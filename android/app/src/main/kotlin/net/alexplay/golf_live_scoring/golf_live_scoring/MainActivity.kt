b69850345c6b22d58ad66f7e00eca44e279ebeac
aleh panko <oleg.panko@banuba.com>
2025-01-15 00:54:37 +0300

    initial commit

---
+++ android/app/src/main/kotlin/net/alexplay/golf_live_scoring/golf_live_scoring/MainActivity.kt
@@ -0,0 +1,5 @@
+package net.alexplay.golf_live_scoring.golf_live_scoring
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:golf_live_scoring/golf_app.dart';
+
+void main() {
+  runApp(const GolfApp());
+}
